7670e95059a5bc197bc935693bc19b3a01f3210b
Dmitry Levchenko <wirwl@hotmail.com>
2019-08-26 21:53:34 +0600

    Improved component rate-button: redesign with css, delete js code

--- SRC/pages/ui-kit/fe/fe.js
+++ SRC/pages/ui-kit/fe/fe.js
@@ -16,7 +16,6 @@ import '../../../components/dd-input/dd-input.js'
 import '../../../components/fdd-input/fdd-input.js'
 import '../../../components/comfort-input/comfort-input.js'
 import '../../../components/expandable-checkbox-list/expandable-checkbox-list.js'
-import '../../../components/rate-button/rate-button.js'
 
 $(document).ready(() => {
   var $qs = $(document).find('.iqdropdown');
