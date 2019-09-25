b55d1068142201574de2bba1af7db0fa6edaa324
Dmitry Levchenko <wirwl@hotmail.com>
2019-09-25 18:22:38 +0600

    Check fe.html(form elements) in w3 validator - fix all errors

--- SRC/pages/ui-kit/cards/cards.js
+++ SRC/pages/ui-kit/cards/cards.js
@@ -2,6 +2,28 @@ import './cards.scss';
 
 
 
+
+
+import '../../../../node_modules/jquery/dist/jquery.js'
+import '../../../jquery/plugins/jquery.maskedinput.min.js'
+
+
+
+// import '../../../../node_modules/jquery/dist/jquery.js'
+// import '../../../jquery/plugins/jquery.maskedinput.min.js'
+// import '../../../../node_modules/ion-rangeslider/js/ion.rangeSlider'
+// import '../../../components/masked-input/masked-input.js'
+import '../../../components/dd-input/dd-input.js'
+// import '../../../components/fdd-input/fdd-input.js'
+// import '../../../components/comfort-input/comfort-input.js'
+// import '../../../components/expandable-checkbox-list/expandable-checkbox-list.js'
+// import '../../../components/like-button/like-button.js'
+import '../../../components/guests-input/guests-input.js'
+// import '../../../components/range-slider/range-slider.js';
+// import '../../../components/pagination/pagination.js';
+import '../../../components/item-quantity-dropdown/item-quantity-dropdown.js'
+
+
 $(document).ready(() => {
   
 });
