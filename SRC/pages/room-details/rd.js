f3e0534e3fd71ee4a3b157191948ca9322eb403f
Dmitry Levchenko <wirwl@hotmail.com>
2019-11-16 20:44:06 +0600

    Rename message-control to feedback

--- SRC/pages/room-details/rd.js
+++ SRC/pages/room-details/rd.js
@@ -1,8 +1,21 @@
 import './rd.scss'
 
 import '../../components/logo/logo.svg'
+import '../../components/room-images/image1.jpg'
+import '../../components/room-images/image2.jpg'
+import '../../components/room-images/image3.jpg'
 
 import '../../favicons/favicons'
 
 import '../../../node_modules/jquery/dist/jquery'
 import '../../components/header-menu/header-menu'
+
+import '../../../node_modules/flexslider/jquery.flexslider'
+
+$(document).ready(function(){
+  $('.flexslider').flexslider({
+    animation: "fade",    
+    touch: true
+  });
+})
+
