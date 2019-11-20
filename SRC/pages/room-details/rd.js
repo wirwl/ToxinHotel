600fa296bf616cb216360af3a44cbfc071db24fa
Dmitry Levchenko <wirwl@hotmail.com>
2019-11-20 22:10:26 +0600

    Added new components: donut-chart, legend, legend-list

--- SRC/pages/room-details/rd.js
+++ SRC/pages/room-details/rd.js
@@ -5,12 +5,22 @@ import '../../components/room-images/image1.jpg'
 import '../../components/room-images/image2.jpg'
 import '../../components/room-images/image3.jpg'
 
+import '../../images/avatar.jpg'
+import '../../images/avatar2.jpg'
+
 import '../../favicons/favicons'
 
 import '../../../node_modules/jquery/dist/jquery'
+import '../../../node_modules/air-datepicker/dist/js/datepicker.js'
+import '../../components/datepicker/datepicker.js'
+import '../../components/datepicker-input/datepicker-input.js'
+import '../../components/order-info-form/order-info-form.js'
 import '../../components/header-menu/header-menu'
-
 import '../../../node_modules/flexslider/jquery.flexslider'
+import '../../components/item-quantity-dropdown/item-quantity-dropdown'
+import '../../components/guests-input/guests-input'
+import '../../components/donut-chart/donut-chart'
+
 
 $(document).ready(function(){
   $('.flexslider').flexslider({
