c17bd01587dbf3a0765896c6090fb3d8c370e15e
Dmitry Levchenko <wirwl@hotmail.com>
2020-04-08 16:13:30 +0600

    Fix code with FSD standarts: add alt attribute in image tag

--- SRC/pages/ui-kit/fe/fe.js
+++ SRC/pages/ui-kit/fe/fe.js
@@ -22,6 +22,12 @@ $(document).ready(() => {
   //Only for watch pixel perfect for range-slider;
   $('.js-range-slider__values').text('5 000₽ - 10 000₽');
   let datepicker = $('#fe-di2').data('datepicker');
+
+  let rs = $('.js-range-slider__input').data('ionRangeSlider');  
+  rs.update({
+    from: 4000,
+    to: 8000
+  });  
   datepicker.update('minDate', null);
   datepicker.selectDate([new Date("2019-08-19")]);
 });
