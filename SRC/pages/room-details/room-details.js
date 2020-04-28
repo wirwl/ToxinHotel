9ac3f685149ddbb15ecde41133e5b028b0009c05
Dmitry Levchenko <wirwl@hotmail.com>
2020-04-28 14:53:28 +0600

    Fix problems in room-images #4

--- SRC/pages/room-details/room-details.js
+++ SRC/pages/room-details/room-details.js
@@ -2,7 +2,8 @@ import './room-details.scss'
 
 import '../../favicons/favicons'
 
-import '../../../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.js'
+import '../../../node_modules/air-datepicker/dist/js/datepicker'
+import '../../../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min'
 
 const fonts = require.context('../../fonts', true, /\.css$/ );
 fonts.keys().forEach(fonts);
