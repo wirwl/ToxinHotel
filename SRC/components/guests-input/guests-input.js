7d3acfc424f4b76c4dcf91e4f229dfab1261d269
Dmitry Levchenko <wirwl@hotmail.com>
2020-04-29 23:05:54 +0600

    Redesign js code in header-menu component #7

--- SRC/components/guests-input/guests-input.js
+++ SRC/components/guests-input/guests-input.js
@@ -2,6 +2,7 @@ class GuestsInput {
   constructor(data) {
     const { rootElementClass, guests, babies, placeholder } = data;
 
+    this.data = data;
     this.$rootElement = $(rootElementClass);
     this.guests = guests;
     this.babies = babies;
@@ -9,17 +10,20 @@ class GuestsInput {
 
     this.$iqdropdown = this.$rootElement.find('.iqdropdown');
     if (this.$iqdropdown.length)
-      this.init_Plugin_ItemQuantityDropdown();
+      this.init_Plugin_ItemQuantityDropdown(this.$iqdropdown);
+
+    this.$buttonClear = this.$rootElement.find('.guests-input__button-simple-clear').find('.button-simple');
+    this.$buttonClear.on('click.buttonClear', this.onButtonClick.bind(this));
   }
 
-  init_Plugin_ItemQuantityDropdown() {
-    this.$iqdropdown.iqDropdown({
+  init_Plugin_ItemQuantityDropdown($iqdropdown) {
+    $iqdropdown.iqDropdown({
       setSelectionText: (itemCount, totalItems) => {
         let result = this.placeholder;
         let babiesCount = itemCount[this.babies.id];
         let guestsCount = totalItems - babiesCount;
 
-        if (totalItems > 0) {          
+        if (totalItems > 0) {
           let wordGuests = this.guests.singular;
           if (guestsCount > 0) {
             const [number2, number5] = this.guests.plurals;
@@ -38,6 +42,19 @@ class GuestsInput {
         return result;
       },
     })
+  }
+
+  onButtonClick(e) {
+    console.log(e.currentTarget);
+    const $button = $(e.currentTarget);
+    const $iqdropdown = $button.closest('.iqdropdown');
+    $iqdropdown.find('.iqdropdown-content').removeClass('iqdropdown-content');
+    $iqdropdown.find('.iqdropdown-item-controls').remove();
+    $iqdropdown.off();
+    $iqdropdown.find('.iqdropdown-menu-option').removeData("defaultcount");
+    $iqdropdown.find('.iqdropdown-menu-option').removeAttr("data-defaultcount");    
+    this.init_Plugin_ItemQuantityDropdown($iqdropdown);
+    $iqdropdown.toggleClass('menu-open');
   }
 }
 
--- SRC/components/header-menu/header-menu.js
+++ SRC/components/header-menu/header-menu.js
@@ -1,45 +1,51 @@
-function CloseAllOpenMenuItems(li) {
-  let ulChild = li.find('> ul');
-  ulChild.css('display', 'none');
-  ulChild.children().each(function () {
-    CloseAllOpenMenuItems($(this))
-  })
-}
-
-$('.js-menu__link').on('click.menu', function (e) {
-  let li = $(this).parent();
-  let ul = li.find('> ul');
-  let ulParent = li.parent();
-  ulParent.children().each(function () {
-    let item = $(this);
-    if (item.is(li)) {
-      if (ul.css('display') === 'none')
-        ul.css('display', 'block');
-      else
-        ul.css('display', 'none');
-    }
-    else CloseAllOpenMenuItems(item);
-  })
-});
-
-$('.js-menu__icon').on('click.menu', function (e) {
-  if ($(this).hasClass('header-menu_show')) {
-    $(this).removeClass('header-menu_show');
-    $(this).addClass('header-menu_hide');
+class HeaderMenu {
+  constructor() {
+    $('.js-menu__link').on('click.menu', this.onClick_link.bind(this));
+    $('.js-menu__icon').on('click.menu', this.onClick_icon);
+    $('.js-menu .js-menu__list').on('mouseleave.menu', this.onMouseleave_list.bind(this));
   }
-  else {
-    $(this).removeClass('header-menu_hide');
-    $(this).addClass('header-menu_show');
-  }
-})
 
-$('.js-menu > .js-menu__list').on('mouseleave.menu', function () {
-  if ($(this).parent().find('.header-menu_show')) {
-    $(this).children().each(function () {
-      CloseAllOpenMenuItems($(this));
+  CloseAllOpenMenuItems($li) {
+    let ulChild = $li.find('> ul');
+    ulChild.css('display', 'none');
+    ulChild.children().each((index, element) => { this.CloseAllOpenMenuItems($(element)) })
+  }
+
+  onClick_link(e) {
+    let li = $(e.currentTarget).parent();
+    let ul = li.find('> ul');
+    let ulParent = li.parent();
+    ulParent.children().each((index, element) => {
+      let item = $(element);
+      if (item.is(li)) {
+        if (ul.css('display') === 'none') ul.css('display', 'block'); else ul.css('display', 'none');
+      } else
+        this.CloseAllOpenMenuItems(item);
     })
-    $(this).parent().find('.header-menu__icon').removeClass('header-menu_show');
-    $(this).parent().find('.header-menu__icon').addClass('header-menu_hide');
-
   }
-})
+
+  onClick_icon(e) {
+    const $icon = $(e.currentTarget);
+    if ($icon.hasClass('header-menu_show')) {
+      $icon.removeClass('header-menu_show');
+      $icon.addClass('header-menu_hide');
+    }
+    else {
+      $icon.removeClass('header-menu_hide');
+      $icon.addClass('header-menu_show');
+    }
+  }
+
+  onMouseleave_list(e) {
+    const $list = $(e.currentTarget);
+    if ($list.parent().find('.header-menu_show')) {
+      $list.children().each((index, element) => {
+        this.CloseAllOpenMenuItems($(element));
+      })
+      $list.parent().find('.header-menu__icon').removeClass('header-menu_show');
+      $list.parent().find('.header-menu__icon').addClass('header-menu_hide');
+
+    }
+  }
+}
+new HeaderMenu();
