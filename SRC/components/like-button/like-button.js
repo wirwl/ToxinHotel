a835af297a6bd0eb57fa926aacef78eedccd5c3a
Dmitry Levchenko <wirwl@hotmail.com>
2020-04-30 18:24:23 +0600

    Redesign js code in like-button component #7

--- SRC/components/like-button/like-button.js
+++ SRC/components/like-button/like-button.js
@@ -1,4 +1,14 @@
-$('.js-like-button__input').on('click.like-button', function () {
-  let $count = $(this).parent().find('.js-like-button__count');
-  ($(this).is(':checked')) ? $count.text(+$count.text() + 1) : $count.text(+$count.text() - 1)
-})
+class LikeButton {
+  constructor(rootElementClass) {
+    this.$rootElementClass = $(rootElementClass);
+    this.$rootElementClass.on('click.like-button', this.onClick_button)
+  }
+
+  onClick_button(e) {
+    const $button = $(e.currentTarget);
+    const $count = $button.parent().find('.js-like-button__count');
+    ($button.is(':checked')) ? $count.text(+$count.text() + 1) : $count.text(+$count.text() - 1);
+  }
+}
+
+new LikeButton('.js-like-button__input');
