4e7b86c6310ffe17979d5e599a9f6fd69a715f6a
wirwl <wirwl@hotmail.com>
2020-09-21 17:50:33 +0600

    Remove extra code in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,42 +6,94 @@ const autoprefixer = require('autoprefixer');
 const pxtorem = require('postcss-pxtorem');
 const discardduplicates = require('postcss-discard-duplicates');
 const flexbugsfixes = require('postcss-flexbugs-fixes');
-const merge = require('webpack-merge');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const sass = require('sass');
 
-let devServer;
-process.noDeprecation = true;
-process.traceDeprecation = true;
-
-module.exports = ((env, argv) => {
+module.exports = (_, argv) => {
   const isDev = argv.mode === 'development';
   const dtValue = isDev ? 'source-map' : 'none';
 
-  // ------------common values for all configs--------------------------------
-  const common = {
-    devServer: {
-      stats: 'errors-only',
-      before(app, server) {
-        devServer = server;
-      },
-    },
+  return {
     performance: { hints: false },
     devtool: dtValue,
+    entry: {
+      index: './SRC/pages/index/index.js',
+      landing: './SRC/pages/landing/landing.js',
+      'room-details': './SRC/pages/room-details/room-details.js',
+      'search-room': './SRC/pages/search-room/search-room.js',
+      'sign-in': './SRC/pages/sign-in/sign-in.js',
+      'sign-up': './SRC/pages/sign-up/sign-up.js',
+      cards: './SRC/pages/ui-kit/cards/cards.js',
+      'colors-type': './SRC/pages/ui-kit/colors-type/colors-type.js',
+      'form-elements': './SRC/pages/ui-kit/form-elements/form-elements.js',
+      'headers-footers': './SRC/pages/ui-kit/headers-footers/headers-footers.js',
+    },
+    output: {
+      path: path.resolve(__dirname, 'Result'),
+      filename: 'js/[name].js',
+    },
+    plugins: [      
+      ((isDev) ? ()=>{return null} : (new CleanWebpackPlugin())),
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+      }),
+      new FriendlyErrorsWebpackPlugin(),
+
+      new MiniCssExtractPlugin({ filename: 'css/[name].css' }),
+
+      new HtmlWebpackPlugin({
+        template: './SRC/pages/index/index.pug', filename: 'index.html', chunks: ['index'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './SRC/pages/landing/landing.pug', filename: 'pages/landing.html', chunks: ['landing'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './SRC/pages/room-details/room-details.pug', filename: 'pages/room-details.html', chunks: ['room-details'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './SRC/pages/search-room/search-room.pug', filename: 'pages/search-room.html', chunks: ['search-room'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './SRC/pages/sign-in/sign-in.pug', filename: 'pages/sign-in.html', chunks: ['sign-in'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './SRC/pages/sign-up/sign-up.pug', filename: 'pages/sign-up.html', chunks: ['sign-up'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './SRC/pages/ui-kit/cards/cards.pug', filename: 'pages/ui-kit/cards.html', chunks: ['cards'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './SRC/pages/ui-kit/colors-type/colors-type.pug', filename: 'pages/ui-kit/colors-type.html', chunks: ['colors-type'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './SRC/pages/ui-kit/form-elements/form-elements.pug', filename: 'pages/ui-kit/form-elements.html', chunks: ['form-elements'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './SRC/pages/ui-kit/headers-footers/headers-footers.pug', filename: 'pages/ui-kit/headers-footers.html', chunks: ['headers-footers'],
+      }),
+    ],
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            name: 'commons',
+            chunks: 'initial',
+            minChunks: 2,
+            minSize: 0,
+          },
+        },
+      },
+    },
     module: {
       rules: [
         {
           test: /\.pug$/,
+          include: /pages/,
           use: [
-            {
-              loader: 'html-loader',
-              options: {
-                // attrs: ['img:src'],
-                attrs: false,
-                cache: true,
-              },
-            },
+            'html-loader?attrs=false',
             `pug-html-loader?pretty=${isDev}`,
           ],
         },
@@ -95,6 +147,7 @@ module.exports = ((env, argv) => {
         },
         {
           test: /\.m?js$/,
+          include: /(components|pages)/,
           exclude: /(node_modules)/,
           use: {
             loader: 'babel-loader',
@@ -107,21 +160,10 @@ module.exports = ((env, argv) => {
         },
         {
           test: /\.(png|jpe?g|gif|svg)$/,
-          include: /(components|pages)/,
+          include: /components/,
           loader: 'file-loader',
           options: {
             context: 'SRC',
-            outputPath: 'images',
-            name: '[path][name].[ext]',
-          },
-        },
-        {
-          test: /\.(png|jpe?g|gif|svg)$/,
-          include: /(images)/,
-          exclude: /(components)/,
-          loader: 'file-loader',
-          options: {
-            context: 'SRC\\images',
             outputPath: 'images',
             name: '[path][name].[ext]',
           },
@@ -146,82 +188,7 @@ module.exports = ((env, argv) => {
             name: '[path]/[name].[ext]',
           },
         },
-      ], // rules
-    },
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          commons: {
-            name: 'commons',
-            chunks: 'initial',
-            minChunks: 2,
-            minSize: 0,
-          },
-        },
-      },
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery',
-      }),
-      new FriendlyErrorsWebpackPlugin(),
-    ],
-  };
-
-  return merge({}, common, {
-    entry: {
-      index: './SRC/pages/index/index.js',
-      landing: './SRC/pages/landing/landing.js',
-      'room-details': './SRC/pages/room-details/room-details.js',
-      'search-room': './SRC/pages/search-room/search-room.js',
-      'sign-in': './SRC/pages/sign-in/sign-in.js',
-      'sign-up': './SRC/pages/sign-up/sign-up.js',
-      cards: './SRC/pages/ui-kit/cards/cards.js',
-      'colors-type': './SRC/pages/ui-kit/colors-type/colors-type.js',
-      'form-elements': './SRC/pages/ui-kit/form-elements/form-elements.js',
-      'headers-footers': './SRC/pages/ui-kit/headers-footers/headers-footers.js',
-    },
-    output: {
-      path: path.resolve(__dirname, 'Result'),
-      filename: 'js/[name].js',
-    },
-    plugins: [
-
-      new MiniCssExtractPlugin({ filename: 'css/[name].css' }),
-
-      new HtmlWebpackPlugin({
-        template: './SRC/pages/index/index.pug', filename: 'index.html', chunks: ['index'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './SRC/pages/landing/landing.pug', filename: 'pages/landing.html', chunks: ['landing'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './SRC/pages/room-details/room-details.pug', filename: 'pages/room-details.html', chunks: ['room-details'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './SRC/pages/search-room/search-room.pug', filename: 'pages/search-room.html', chunks: ['search-room'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './SRC/pages/sign-in/sign-in.pug', filename: 'pages/sign-in.html', chunks: ['sign-in'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './SRC/pages/sign-up/sign-up.pug', filename: 'pages/sign-up.html', chunks: ['sign-up'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './SRC/pages/ui-kit/cards/cards.pug', filename: 'pages/ui-kit/cards.html', chunks: ['cards'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './SRC/pages/ui-kit/colors-type/colors-type.pug', filename: 'pages/ui-kit/colors-type.html', chunks: ['colors-type'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './SRC/pages/ui-kit/form-elements/form-elements.pug', filename: 'pages/ui-kit/form-elements.html', chunks: ['form-elements'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './SRC/pages/ui-kit/headers-footers/headers-footers.pug', filename: 'pages/ui-kit/headers-footers.html', chunks: ['headers-footers'],
-      }),
-    ],
-  });
-});
+      ]
+    }
+  }
+};
