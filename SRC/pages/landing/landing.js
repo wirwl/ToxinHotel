ec4682316786e07203c368ab7169a22c81cc57ec
Dmitry <wirwl@hotmail.com>
2021-01-13 17:25:09 +0600

    Remove comments in landins.js

--- SRC/pages/landing/landing.js
+++ SRC/pages/landing/landing.js
@@ -1,7 +1,7 @@
 import '../../components/pages/landing/landing';
 
-// const fonts = require.context('../../fonts', true, /\.css$/);
-// fonts.keys().forEach(fonts);
+const fonts = require.context('../../fonts', true, /\.css$/);
+fonts.keys().forEach(fonts);
 
-// const imports = require.context('../../components/', true, /^(?!..pages).*(scss|js)$/);
-// imports.keys().forEach(imports);
+const imports = require.context('../../components/', true, /^(?!..pages).*(scss|js)$/);
+imports.keys().forEach(imports);
